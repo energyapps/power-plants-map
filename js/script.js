bd9e0182adbb503f9bf37132cb7c1737a355b316
Daniel Wood <daniel.j.wood11@gmail.com>
2014-10-27 09:56:30 -0400

    responsive updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-var width = 960,
-    height = 600;
+var margin = 40,
+    width = parseInt(d3.select("#map_container").style("width")) - margin*2,
+    
+    height = parseInt(d3.select("#map_container").style("height")) - margin*2;
+
+// var width = 960,
+//     height = 600;
 
 
 var projection = d3.geo.albersUsa();
@@ -7,9 +12,9 @@ var projection = d3.geo.albersUsa();
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = d3.select("#map_container")
+    .attr("width", width + margin*2)
+    .attr("height", height + margin*2);
 
 var radius = d3.scale.sqrt()
     .domain([0, 1000])
@@ -39,19 +44,54 @@ d3.json("js/us_10m_topo4.json", function(error, us) {
     .data(topojson.feature(us, us.objects.us_10m).features)
     .enter().append("path")
       .attr("class", function(d) {return "state " + d.id; })
-      .attr("d", path);
+     
+
 
   svg.append("path")
     .datum(topojson.mesh(us, us.objects.us_10m, function(a,b) {return a !== b;}))
-    .attr("d", path)
     .attr("class", "state-boundary")
 
   svg.append("g")
     .attr("class", "bubble")
   .selectAll("circle")
     .data(topojson.feature(us, us.objects.us_10m).features
-      .sort(function(a, b) { return b.properties.nat_gas - a.properties.nat_gas; }))
+      .sort(function(a, b) { return b.properties.total - a.properties.total; }))
   .enter().append("circle")
-    .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
-    .attr("r", function(d) { return radius(d.properties.nat_gas)});
-});
+    .attr("transform", function(d) { 
+    	return "translate(" + path.centroid(d) + ")"; })
+    .attr("r", function(d) { return radius(d.properties.total)});
+
+
+    function resize() {
+	    var width = parseInt(d3.select("#map_container").style("width")) - margin*2,
+	    height = parseInt(d3.select("#map_container").style("height")) - margin*2;    	
+     	// width = $(window).width();
+
+// console.log(path.centroid(d));
+
+     	projection
+	     	.scale(width)
+	     	.translate([width / 2, height / 2])
+
+    	svg.selectAll('path.state')
+    		.attr("d", path);
+
+    	svg.selectAll('path.state-boundary')
+    		.attr("d", path);
+
+    	svg.selectAll("circle")
+    		.data(topojson.feature(us, us.objects.us_10m).features)
+			.attr("transform", function(d) { 
+				console.log(path.centroid(d));
+    			return "translate(" + path.centroid(d) + ")"; })
+    		.attr("r", function(d) { return radius(d.properties.total)});
+
+
+
+	    console.log(width);
+	    console.log(height)
+    }
+   	resize();
+    d3.select(window).on('resize', resize); 
+    	resize();
+	});
