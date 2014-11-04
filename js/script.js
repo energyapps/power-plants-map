c3b12296538b193ab1de7f886f01bc0787cf45b9
Daniel Wood <daniel.j.wood11@gmail.com>
2014-11-04 17:00:50 -0500

    changing arcs to highlight

--- js/script.js
+++ js/script.js
@@ -137,38 +137,39 @@ d3.json("js/us_10m_topo4.json", function(error, us) {
     }
 
     function tooltip(d) {     
-    width = parseInt(d3.select("#master_container").style("width")) - margin*2,
-    // console.log(width)
+      width = parseInt(d3.select("#master_container").style("width")) - margin*2,
 
-
-        d3.select("#tooltip").remove();
-        d3.selectAll(".arc").remove();
-        d3.selectAll(".tip-text").remove();
-        d3.selectAll(".tip-text2").remove();        
-        d3.selectAll(".tip-text3").remove();        
-
+      // Remove everything and start over.
+      remover();
       
       var data = d;
       centroid = path.centroid(data);
 
-    if (width > 700) {  
-      if (centroid[1] < 250) {
-        centroid_adjusted = [(centroid[0]-radius - 5),(centroid[1]+25)];
+      if (width > 700) {  
+        if (centroid[1] < 250) {
+          centroid_adjusted = [(centroid[0]-radius - 5),(centroid[1]+25)];
+        } else {
+          centroid_adjusted = [(centroid[0]-radius - 5),(centroid[1]-(2 * radius + 80))];
+        };
+      }
+      else if (width > 480) {
+        if (centroid[0] < width / 2) {
+          centroid_adjusted = [(width - 175),(5)];        
+        } else {
+          centroid_adjusted = [(5),(5)];               
+        };
       } else {
-        centroid_adjusted = [(centroid[0]-radius - 5),(centroid[1]-(2 * radius + 80))];
+        if (centroid[0] < 200) {
+          centroid_adjusted = [(width - 175),(5)];        
+        } else {
+          centroid_adjusted = [(5),(5)];               
+        };
       };
-    }
-    else {
-      if (centroid[0] < width / 2) {
-        centroid_adjusted = [(width - 175),(5)];        
-      } else {
-        centroid_adjusted = [(5),(5)];               
-      };
-    };
 
         tip_text  = [(centroid_adjusted[0] + radius + 5),(centroid_adjusted[1] + 20)];
         tip_text2  = [(centroid_adjusted[0] + radius + 5),(centroid_adjusted[1] + 40)];
         pie_center = [(centroid_adjusted[0] + radius + 5),(centroid_adjusted[1]+(radius + 40))];
+        tip_close = [(centroid_adjusted[0] + radius + 5),(centroid_adjusted[1]+(radius + 45))];
 
 // Create array for pie charts here!!!!!!!!!!!!!!!!!!!!!!! put in memory and use laterZZzzzZzzZzzzZZzzZZZz
       var data_array = [{type: "Biofuels", value: data.properties.biofuels, x:centroid_adjusted[0], y:centroid_adjusted[1]},
@@ -204,12 +205,21 @@ d3.json("js/us_10m_topo4.json", function(error, us) {
       svg
         .append("text")
         .attr("class","tip-text2")
-        // .attr("class","tip-text2")
         .text(function(d){
             return "Total: " + data.properties.total + " BTU";
         })
         .attr("transform", function() { 
           return "translate(" + tip_text2 + ")"; });
+
+      svg.append("g")
+        .attr("class", "closer")
+        .attr("transform", function(){
+          return "translate(" + tip_close + ")";
+        })
+          .append("text")
+          .attr("class", "tip-text2")
+          .text("CLOSE").on("click", remover);
+
 
 
 var tip_position = [(centroid_adjusted[0] + 85),(centroid_adjusted[1] + 205)];
@@ -287,10 +297,24 @@ var tip_position = [(centroid_adjusted[0] + 85),(centroid_adjusted[1] + 205)];
       }
     // }        centroid_adjusted = [(centroid[0]-radius),(centroid[1]+25)];
 
+    function remover() {
+      d3.select("#tooltip").remove();
+      d3.selectAll(".arc").remove();
+      d3.selectAll(".tip-text").remove();
+      d3.selectAll(".tip-text2").remove();        
+      d3.selectAll(".tip-text3").remove();     
+    }
+
+    function clickMe(){alert("I've been clicked!")}
+
+
 
    	resize();
     d3.select(window).on('resize', resize); 
     d3.selectAll("circle.bubble").on('mouseover', tooltip);
+    // d3.selectAll(".closer")
+    //   .selectAll('.tip-text2')
+    //   .on('click', clickMe)
 
        // Doesn't work the below vvvv
     // d3.selectAll("g.arc").on('mouseout', function(){d3.selectAll(".tip-text2").remove();})
