3a4cafa8f23091c64fe002a48f465972e21cbcbb
Daniel Wood <daniel.j.wood11@gmail.com>
2014-11-05 12:52:24 -0500

    add attribution

--- js/script.js
+++ js/script.js
@@ -81,33 +81,33 @@ d3.json("js/us_10m_topo4.json", function(error, us) {
      	// width = $(window).width();
 
     // Smaller viewport
-      // if (width <= 200) {
-      //   // var radius2 = d3.scale.sqrt()  
-      //   //   .domain([0, 3000])
-      //   //   .range([5, 15]);    
+      if (width <= 500) {
+        // var radius2 = d3.scale.sqrt()  
+        //   .domain([0, 3000])
+        //   .range([5, 15]);    
 
-      //   // projection
-      //   //   .scale(width * 1.1)
-      //   //   .translate([width / 2, height / 2])             
-      // } 
-      // // full viewport
-      // else {
-      //   var radius2 = d3.scale.sqrt()  
-      //     .domain([0, 1000])
-      //     .range([(width / 110), (width / 45)]); 
+        projection
+          .scale(width * 1.2)
+          .translate([width / 2, ((height / 2) + 10)])             
+      } 
+      // full viewport
+      else {
+        // var radius2 = d3.scale.sqrt()  
+        //   .domain([0, 1000])
+        //   .range([(width / 110), (width / 45)]); 
 
-      //   projection
-      //     .scale(width)
-      //     .translate([width / 2, height / 2])   
-      // };
+        projection
+          .scale(width)
+          .translate([width / 2, height / 2])   
+      };
 
         var radius2 = d3.scale.sqrt()  
           .domain([0, 1000])
           .range([(width / 110), (width / 45)]); 
 
-        projection
-          .scale(width)
-          .translate([width / 2, height / 2])  
+        // projection
+        //   .scale(width)
+        //   .translate([width / 2, height / 2])  
 
       legend.append("circle")
 
@@ -115,8 +115,15 @@ d3.json("js/us_10m_topo4.json", function(error, us) {
           .attr("dy", "1.3em")
           .text(d3.format(".1s"));
 
-      legend        
-        .attr("transform", "translate(" + (width - 65) + "," + (height - 15) + ")");
+// if (width < 500) {
+//   legend        
+//     .attr("transform", "translate(" + (width - 65) + "," + (height - 15) + ")");
+// } else {
+//   legend        
+//     .attr("transform", "translate(" + (width - 65) + "," + (height - 15) + ")");
+// };
+  legend        
+    .attr("transform", "translate(" + (width - (radius2(10000) + 10)) + "," + (height - 10) + ")");
 
       legend.selectAll("circle")
         .attr("cy", function(d) { return -radius2(d); })
